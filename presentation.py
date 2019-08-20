085eb6a7c37f24f2cd6fc794c4b1976de84ebb04
iamntbb <54301294+iamntbb@users.noreply.github.com>
2019-08-20 09:59:11 +0530

    Update presentation.py

--- presentation.py
+++ presentation.py
@@ -54,6 +54,7 @@ x = (P @ (y)) * F**0.5
 plt.plot(x[0,:len],x[1,:len],color='r',label='Hyperbola')
 plt.plot(x[0,len+1:],x[1,len+1:],color='r')
 
+#Plotting the Normal
 P = np.array([8,3*(3**0.5)]) 
 B = np.array([25/2,0]) 
 len =10
@@ -64,6 +65,7 @@ for i in range(len):
   temp1 = P + lam[i]*(B-P)
   x_PB[:,i]= temp1.T
   
+#Plotting the Tangent
 C = np.array([2,0]) 
 len =10
 
@@ -72,10 +74,11 @@ lam = np.linspace(-0.5,1,len)
 for i in range(len):
   temp1 = P + lam[i]*(C-P)
   x_PC[:,i]= temp1.T 
-  
+#PLotting the point on the hyperbola 
 plt.plot(P[0], P[1], 'o')
 plt.text(P[0] * (1 + 0.1), P[1] * (1 - 0.1) , 'P') 
-  
+ 
+#PLotting the Lines on the hyperbola
 plt.plot(x_PB[0,:],x_PB[1,:],label='$Normal$')
 plt.plot(x_PC[0,:],x_PC[1,:],label='$Tangent$')
 plt.grid() 
