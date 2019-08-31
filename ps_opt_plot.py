874774db1d4e9847650d609a0830f61dfda92aa9
iamntbb <54301294+iamntbb@users.noreply.github.com>
2019-08-31 14:14:04 +0530

    Update ps_opt_plot.py

--- ps_opt_plot.py
+++ ps_opt_plot.py
@@ -1,15 +1,15 @@
-#Plotting the curve f(x)=x**3-3xc for different values of c
+#Plotting The Curves For f(x)=x**3-3xc
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Defining the cubic curve
-def curve_cubic(a,b,c,d):
-	x=list(range(-10,11))
-	y=[(a*i**3+b*i**2+c*i+d) for i in x]
-	plt.plot(x,y,label='cubic', linestyle='-')
-#Putting different values of c
+#Plotting The Curves For Different Values Of c
 
 #c=2
+def curve_cubic(a,b,c,d,):
+	x=list(range(-10,11))
+	y=[(a*(i**3)+b*(i**2)+c*i+d) for i in x]
+	plt.plot(x,y,label='Cubic Equation For c=2', linestyle='-')
+
 a=1
 b=0
 c=-6
@@ -17,6 +17,10 @@ d=0
 curve_cubic(a,b,c,d)
 
 #c=4
+def curve_cubic(a,b,c,d,):
+	x=list(range(-10,11))
+	y=[(a*(i**3)+b*(i**2)+c*i+d) for i in x]
+	plt.plot(x,y,label='Cubic Equation For c=4', linestyle='-')
 a=1
 b=0
 c=-12
@@ -24,6 +28,10 @@ d=0
 curve_cubic(a,b,c,d)
 
 #c=6
+def curve_cubic(a,b,c,d,):
+	x=list(range(-10,11))
+	y=[(a*(i**3)+b*(i**2)+c*i+d) for i in x]
+	plt.plot(x,y,label='Cubic Equation For c=6', linestyle='-')
 a=1
 b=0
 c=-18
@@ -31,6 +39,10 @@ d=0
 curve_cubic(a,b,c,d)
 
 #c=0
+def curve_cubic(a,b,c,d,):
+	x=list(range(-10,11))
+	y=[(a*(i**3)+b*(i**2)+c*i+d) for i in x]
+	plt.plot(x,y,label='Cubic Equation For c=0', linestyle='-')
 a=1
 b=0
 c=0
@@ -38,6 +50,10 @@ d=0
 curve_cubic(a,b,c,d)
 
 #c=-2
+def curve_cubic(a,b,c,d,):
+	x=list(range(-10,11))
+	y=[(a*(i**3)+b*(i**2)+c*i+d) for i in x]
+	plt.plot(x,y,label='Cubic Equation For c=-2', linestyle='-')
 a=1
 b=0
 c=6
@@ -45,6 +61,10 @@ d=0
 curve_cubic(a,b,c,d)
 
 #c=-4
+def curve_cubic(a,b,c,d,):
+	x=list(range(-10,11))
+	y=[(a*(i**3)+b*(i**2)+c*i+d) for i in x]
+	plt.plot(x,y,label='Cubic Equation For c=-4', linestyle='-')
 a=1
 b=0
 c=12
@@ -52,11 +72,16 @@ d=0
 curve_cubic(a,b,c,d)
 
 #c=-6
+def curve_cubic(a,b,c,d,):
+	x=list(range(-10,11))
+	y=[(a*(i**3)+b*(i**2)+c*i+d) for i in x]
+	plt.plot(x,y,label='Cubic Equation For c=-6', linestyle='-')
 a=1
 b=0
 c=18
 d=0
 curve_cubic(a,b,c,d)
 
+plt.legend(loc='best')
 plt.grid()
 plt.show()
